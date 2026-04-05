9bef45caca57fe080e133b714021a01ebf73b93e
Vitor Chaves <vitorclcoelho@protonmail.com>
2026-04-05 10:37:59 -0300

    fix: use camelCase JSON tags in telemetry DTOs

--- internal/modules/telemetry/application/dto/telemetry.go
+++ internal/modules/telemetry/application/dto/telemetry.go
@@ -11,15 +11,15 @@ type TelemetryOutput struct {
 	Time           time.Time `json:"time"`
 	RPM            *int      `json:"rpm,omitempty"`
 	Speed          *int      `json:"speed,omitempty"`
-	CoolantTemp    *float64  `json:"coolant_temp,omitempty"`
-	IntakeTemp     *float64  `json:"intake_temp,omitempty"`
-	EngineLoad     *float64  `json:"engine_load,omitempty"`
-	ThrottlePos    *float64  `json:"throttle_pos,omitempty"`
-	FuelLevel      *float64  `json:"fuel_level,omitempty"`
-	FuelTrimShort  *float64  `json:"fuel_trim_short,omitempty"`
-	FuelTrimLong   *float64  `json:"fuel_trim_long,omitempty"`
+	CoolantTemp    *float64  `json:"coolantTemp,omitempty"`
+	IntakeTemp     *float64  `json:"intakeTemp,omitempty"`
+	EngineLoad     *float64  `json:"engineLoad,omitempty"`
+	ThrottlePos    *float64  `json:"throttlePos,omitempty"`
+	FuelLevel      *float64  `json:"fuelLevel,omitempty"`
+	FuelTrimShort  *float64  `json:"fuelTrimShort,omitempty"`
+	FuelTrimLong   *float64  `json:"fuelTrimLong,omitempty"`
 	MAF            *float64  `json:"maf,omitempty"`
-	BatteryVoltage *float64  `json:"battery_voltage,omitempty"`
+	BatteryVoltage *float64  `json:"batteryVoltage,omitempty"`
 }
 
 type TelemetryListOutput struct {
@@ -29,20 +29,20 @@ type TelemetryListOutput struct {
 
 type TelemetrySummaryOutput struct {
 	Bucket            time.Time `json:"bucket"`
-	AvgRPM            *float64  `json:"avg_rpm,omitempty"`
-	MaxRPM            *float64  `json:"max_rpm,omitempty"`
-	AvgSpeed          *float64  `json:"avg_speed,omitempty"`
-	MaxSpeed          *float64  `json:"max_speed,omitempty"`
-	AvgCoolantTemp    *float64  `json:"avg_coolant_temp,omitempty"`
-	MaxCoolantTemp    *float64  `json:"max_coolant_temp,omitempty"`
-	AvgEngineLoad     *float64  `json:"avg_engine_load,omitempty"`
-	AvgMAF            *float64  `json:"avg_maf,omitempty"`
-	AvgBatteryVoltage *float64  `json:"avg_battery_voltage,omitempty"`
+	AvgRPM            *float64  `json:"avgRpm,omitempty"`
+	MaxRPM            *float64  `json:"maxRpm,omitempty"`
+	AvgSpeed          *float64  `json:"avgSpeed,omitempty"`
+	MaxSpeed          *float64  `json:"maxSpeed,omitempty"`
+	AvgCoolantTemp    *float64  `json:"avgCoolantTemp,omitempty"`
+	MaxCoolantTemp    *float64  `json:"maxCoolantTemp,omitempty"`
+	AvgEngineLoad     *float64  `json:"avgEngineLoad,omitempty"`
+	AvgMAF            *float64  `json:"avgMaf,omitempty"`
+	AvgBatteryVoltage *float64  `json:"avgBatteryVoltage,omitempty"`
 }
 
 type DTCOutput struct {
 	Code       string    `json:"code"`
-	DetectedAt time.Time `json:"detected_at"`
+	DetectedAt time.Time `json:"detectedAt"`
 }
 
 type DTCListOutput struct {
